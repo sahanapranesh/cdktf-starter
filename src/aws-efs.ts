18dfe83d672219c6eb170f3a1731bc2741860c1b
Sahana Pranesh <sahana.pranesh@estimateone.com>
2023-02-08 15:42:02 -0500

    Updates to EFS config

--- src/aws-efs.ts
+++ src/aws-efs.ts
@@ -4,10 +4,20 @@ import { EfsMountTarget } from '@cdktf/provider-aws/lib/efs-mount-target';
 import { Fn } from 'cdktf';
 import { Construct } from 'constructs';
 
+export interface AwsEfsAccessPointConfig {
+  posixUid: number;
+  ownerUid: number;
+  ownerGid: number;
+  posixGid: number;
+  rootDirectoryPath: string;
+  creationInfo: string;
+  permissions: string;
+}
 
 export interface AwsEfsConfig {
   subnets: string[];
   securityGroupId: string;
+  accessPointConfig?: AwsEfsAccessPointConfig;
   tags: any;
 }
 
@@ -29,19 +39,26 @@ export class AwsEfs extends Construct {
     this.accessPoint = new EfsAccessPoint(scope, name + '-efs-access-point', {
       fileSystemId: this.efs.id,
       tags: config.tags,
+      posixUser: {
+        gid: config.accessPointConfig ? config.accessPointConfig.posixGid : 0,
+        uid: config.accessPointConfig ? config.accessPointConfig.posixUid : 0,
+      },
+      rootDirectory: {
+        path: config.accessPointConfig ? config.accessPointConfig.rootDirectoryPath : '/',
+        creationInfo: {
+          ownerGid: config.accessPointConfig ? config.accessPointConfig.ownerGid : 0,
+          ownerUid: config.accessPointConfig ? config.accessPointConfig.ownerUid : 0,
+          permissions: config.accessPointConfig ? config.accessPointConfig.permissions : '',
+        },
+      },
     });
 
-    //Works on the assumption that we have atleast 2 subnets where we need to create mount points
-    new EfsMountTarget(scope, name + '-efs-mount-target-1', {
-      fileSystemId: this.efs.id,
-      subnetId: Fn.element(config.subnets, 0),
-      securityGroups: [config.securityGroupId],
-    });
-
-    new EfsMountTarget(scope, name + '-efs-mount-target-2', {
-      fileSystemId: this.efs.id,
-      subnetId: Fn.element(config.subnets, 1),
-      securityGroups: [config.securityGroupId],
-    });
+    for (let i = 0; i < config.subnets.length; i++) {
+      new EfsMountTarget(scope, name + '-efs-mount-target' + i, {
+        fileSystemId: this.efs.id,
+        subnetId: Fn.element(config.subnets, 0),
+        securityGroups: [config.securityGroupId],
+      });
+    }
   }
 }
